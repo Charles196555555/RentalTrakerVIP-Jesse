04ce308b0f447884cbac74af747f2aa7348e6a74
Charles196555555 <charles_v_johnson_1965@yahoo.com>
2025-04-19 22:56:45 -0500

    Add homepage with JesseAssistant
    Commit new file

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+src/pages/index.js
+``>
+
+> Make sure to include the full path: `src/pages/index.js`
+
+3. Paste this code:
+
+```js
+import JesseAssistant from '../components/JesseAssistant';
+
+export default function Home() {
+  return <JesseAssistant />;
+}
